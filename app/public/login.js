cb2f009ec3f4322ee45f8bbad8bc9f922cc1dd10
Soymanuelperez <soymanuelperez@outlook.com>
2024-02-14 20:08:25 -0500

    cambio al originar

--- app/public/login.js
+++ app/public/login.js
@@ -1,13 +1,10 @@
 const mensajeError = document.getElementsByClassName("error")[0]
-//Fix para __direname
-import path from 'path';
-import {fileURLToPath} from 'url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 document.getElementById("login").addEventListener("submit",async (e)=>{
   e.preventDefault();
   const user = e.target.children.user.value;
   const password = e.target.children.password.value;
-  const res = await fetch(__dirname +"/api/login",{
+  const res = await fetch("http://localhost:4000/api/login",{
     method:"POST",
     headers:{
       "Content-Type":"application/json"
