7d8e9bb6eef7a88ea97ad390aa651ba9f6ab3641
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-04-01 16:31:22 -0500

    Rutas de habitaciones y cabañas

--- app/index.js
+++ app/index.js
@@ -29,13 +29,40 @@ app.get("/DSM",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pa
 app.get("/Productos",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/Productos.html"));
 app.get("/hospedaje",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/mesero/hospedaje.html"));
 app.get("/DS-1",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/DS-1.html"));
-app.get("/HB-1",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-1.html"));
 app.get("/DS-1M",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/mesero/DS-1.html"));
 app.get("/HB-1M",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/mesero/HB-1.html"));
 app.get("/huespedes",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/huespedes.html"))
 app.get("/ventas",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/ventas.html"))
 app.get("/Bar",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/Bar.html"))
 app.get("/Restaurante",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/Restaurante.html"))
+//Habitacions
+app.get("/HB-1",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-1.html"));
+app.get("/HB-2",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-2.html"));
+app.get("/HB-3",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-3.html"));
+app.get("/HB-4",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-4.html"));
+app.get("/HB-5",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-5.html"));
+app.get("/HB-6",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-6.html"));
+app.get("/HB-7",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-7.html"));
+app.get("/HB-8",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-8.html"));
+app.get("/HB-9",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-9.html"));
+app.get("/HB-10",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-10.html"));
+app.get("/HB-11",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-11.html"));
+app.get("/HB-12",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-12.html"));
+app.get("/HB-13",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/HB-13.html"));
+//cabañas
+app.get("/CB-14",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-14.html"));
+app.get("/CB-15",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-15.html"));
+app.get("/CB-16",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-16.html"));
+app.get("/CB-17",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-17.html"));
+app.get("/CB-18",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-18.html"));
+app.get("/CB-19",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-19.html"));
+app.get("/CB-20",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-20.html"));
+app.get("/CB-21",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-21.html"));
+app.get("/CB-22",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-22.html"));
+app.get("/CB-23",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-23.html"));
+app.get("/CB-24",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-24.html"));
+app.get("/CB-25",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/CB-25.html"));
+
 //Rutas POST
 app.post("/api/login",authentication.login);
 app.post("/api/register",authentication.register);
