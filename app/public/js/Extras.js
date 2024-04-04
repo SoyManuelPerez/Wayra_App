a82d145a5557566cd692ca59ceb83502e21b9bc0
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-04-04 08:48:53 -0500

    modificacion en dias de sol

--- app/public/js/Extras.js
+++ app/public/js/Extras.js
@@ -57,6 +57,70 @@ function CExtra14() {
     let nombre = document.getElementById("NExtra15").value;
     localStorage.setItem("NombreX15",nombre);
 }
+function CExtra16() { 
+    let nombre = document.getElementById("NExtra16").value;
+    localStorage.setItem("NombreX16",nombre);
+}
+function CExtra17() { 
+    let nombre = document.getElementById("NExtra17").value;
+    localStorage.setItem("NombreX17",nombre);
+}
+function CExtra18() { 
+    let nombre = document.getElementById("NExtra18").value;
+    localStorage.setItem("NombreX18",nombre);
+}
+v
+function CExtra19() { 
+    let nombre = document.getElementById("NExtra19").value;
+    localStorage.setItem("NombreX19",nombre);
+}
+vv
+function CExtra20() { 
+    let nombre = document.getElementById("NExtra20").value;
+    localStorage.setItem("NombreX20",nombre);
+}
+v
+function CExtra21() { 
+    let nombre = document.getElementById("NExtra21").value;
+    localStorage.setItem("NombreX21",nombre);
+}
+function CExtra22() { 
+    let nombre = document.getElementById("NExtra22").value;
+    localStorage.setItem("NombreX22",nombre);
+}
+function CExtra23() { 
+    let nombre = document.getElementById("NExtra23").value;
+    localStorage.setItem("NombreX23",nombre);
+}
+function CExtra24() { 
+    let nombre = document.getElementById("NExtra24").value;
+    localStorage.setItem("NombreX24",nombre);
+}
+function CExtra25() { 
+    let nombre = document.getElementById("NExtra25").value;
+    localStorage.setItem("NombreX25",nombre);
+}
+function CExtra26() { 
+    let nombre = document.getElementById("NExtra26").value;
+    localStorage.setItem("NombreX26",nombre);
+}
+function CExtra27() { 
+    let nombre = document.getElementById("NExtra27").value;
+    localStorage.setItem("NombreX27",nombre);
+}
+function CExtra28() { 
+    let nombre = document.getElementById("NExtra28").value;
+    localStorage.setItem("NombreX28",nombre);
+}
+function CExtra29() { 
+    let nombre = document.getElementById("NExtra29").value;
+    localStorage.setItem("NombreX29",nombre);
+}
+function CExtra30() { 
+    let nombre = document.getElementById("NExtra30").value;
+    localStorage.setItem("NombreX30",nombre);
+}
+
 
 document.addEventListener("DOMContentLoaded",function(event){
     document.getElementById("Extra1").innerHTML = localStorage.getItem("NombreX1");
@@ -70,8 +134,24 @@ document.addEventListener("DOMContentLoaded",function(event){
     document.getElementById("Extra9").innerHTML = localStorage.getItem("NombreX9");
     document.getElementById("Extra10").innerHTML = localStorage.getItem("NombreX10");
     document.getElementById("Extra11").innerHTML = localStorage.getItem("NombreX11");
-    document.getElementById("Extra21").innerHTML = localStorage.getItem("NombreX12");
-    document.getElementById("Extr3a1").innerHTML = localStorage.getItem("NombreX13");
-    document.getElementById("Ext4ra1").innerHTML = localStorage.getItem("NombreX14");
-    document.getElementById("Ex5tra1").innerHTML = localStorage.getItem("NombreX15");
+    document.getElementById("Extra12").innerHTML = localStorage.getItem("NombreX12");
+    document.getElementById("Extra13").innerHTML = localStorage.getItem("NombreX13");
+    document.getElementById("Extra14").innerHTML = localStorage.getItem("NombreX14");
+    document.getElementById("Extra15").innerHTML = localStorage.getItem("NombreX15");
+    document.getElementById("Extra16").innerHTML = localStorage.getItem("NombreX16");
+    document.getElementById("Extra17").innerHTML = localStorage.getItem("NombreX17");
+    document.getElementById("Extra18").innerHTML = localStorage.getItem("NombreX18");
+    document.getElementById("Extra19").innerHTML = localStorage.getItem("NombreX19");
+    document.getElementById("Extra20").innerHTML = localStorage.getItem("NombreX20");
+    document.getElementById("Extra21").innerHTML = localStorage.getItem("NombreX21");
+    document.getElementById("Extra22").innerHTML = localStorage.getItem("NombreX22");
+    document.getElementById("Extra23").innerHTML = localStorage.getItem("NombreX23");
+    document.getElementById("Extra24").innerHTML = localStorage.getItem("NombreX24");
+    document.getElementById("Extra25").innerHTML = localStorage.getItem("NombreX25");
+    document.getElementById("Extra26").innerHTML = localStorage.getItem("NombreX26");
+    document.getElementById("Extra27").innerHTML = localStorage.getItem("NombreX27");
+    document.getElementById("Extra28").innerHTML = localStorage.getItem("NombreX28");
+    document.getElementById("Extra29").innerHTML = localStorage.getItem("NombreX29");
+    document.getElementById("Extra30").innerHTML = localStorage.getItem("NombreX30");
+
 })
